8af5862f60b68b64e65af2512b090696ffd050cf
Bryan Lim <bryan.lim@duke.edu>
2020-08-24 13:04:47 -0400

    Committing calculator.py before edits

--- calculator.py
+++ calculator.py
@@ -1,6 +1,12 @@
 # hi testing if pyhon syntax works here on Jupyter.
 
 
-a = 1
-b = 3
-print(a + b)
+def interface():
+    print("My Program")
+    print("Options:")
+    print("9 - Quit")
+    choice = input("Enter your choice: ")
+    if choice=='9':
+        return
+   
+interface()
