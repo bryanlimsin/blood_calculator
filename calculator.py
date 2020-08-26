5e2867f19910babedd6e094f8799ee37cba6e4d4
Bryan Lim <bryan.lim@duke.edu>
2020-08-25 22:54:29 -0400

    Separated modules from HDL_driver function and added final output

--- calculator.py
+++ calculator.py
@@ -13,34 +13,55 @@ def interface():
 
         elif choice == "1":
             HDL_driver()
+            return
             
 
 def HDL_driver():
 
     # Get the input
     
-    def input_function():
-        x = input("Enter your HDL value")
-        return x
+    input_to_check = input_function()
 
 
     # Check if HDL is normal
     
-    def check_HDL():
-        HDL_value = input_function()
-        
-        if HDL_value > 60:
-            return "Normal"
-        elif 40 <= HDL_value < 60:
-            return "Borderline Low"
-        else:
-            return "Low"
+    final_output = check_HDL(input_to_check)
 
     # Have an output
 
+    out_analysis(input_to_check, final_output)
 
 
-interface()
+
+####################################################################################
+# Modular functions to be used in HDL_driver()
+
+def input_function():
+    x = input("Enter your HDL value: ")
+    x = int(x)
+    return x
+
+
+# Check if HDL is normal
+
+def check_HDL(y):
+    HDL_value = y
+
+    if HDL_value > 60:
+        return "Normal"
+    elif 40 <= HDL_value < 60:
+        return "Borderline Low"
+    else:
+        return "Low"
+
+# Have an output
+
+def out_analysis(x, y):
+    print("The HDL result is {}".format(x))
+    print("The analysis is {}".format(y))
+
+
+interface() # notice running this function after defining all the modules above is correct 
 
 
 
