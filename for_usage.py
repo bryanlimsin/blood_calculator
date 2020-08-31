9b2b19bfb6f3f9126ec32feb85102082b2e83281
Bryan Lim <bryan.lim@duke.edu>
2020-08-31 12:26:02 -0400

    Utilize zip() to for loop through mutiple lists

--- for_usage.py
+++ for_usage.py
@@ -6,3 +6,12 @@ amounts = [11, 22, 33, 44]
 for i, x in enumerate(foods):
     print("{} - {}".format(amounts[i], x))
 
+
+#########
+# With zip()
+
+
+do_I_like_it = [True, False, False, True]
+
+for y, x, z in zip(amounts, foods, do_I_like_it):
+    print("{} - {} - {}".format(y,x,z))
