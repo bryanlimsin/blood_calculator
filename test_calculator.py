bab1520d7327a9312d59ae6644ac85d2c9494a64
Bryan Lim <bryan.lim@duke.edu>
2020-09-02 13:02:07 -0400

    Added parametrize method to test_calculator,py

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,6 @@
 # Created a virtual environment
 
+"""
 def test_check_HDL():
     from calculator import check_HDL #must it be this syntax
     result = check_HDL(70)   # Enter a value into the function you imported and store it as a result 
@@ -13,7 +14,7 @@ def test_check_HDL():
     result = check_HDL(45)
     expected = "Borderline Low"
     assert result  == expected  # option 1 for multi-stage unit testing of a function you import that can have various outputs
-
+"""
 
 
 
@@ -24,13 +25,13 @@ def test_check_HDL():
 
 import pytest
 
-@pytest.mark.paramterize("HDL_test_result, expected", [
+@pytest.mark.parametrize("HDL_test_result, expected", [
     (70, "Normal"),
     (1, "Low"),
     (45, "Borderline Low"),
     ])
-
+def test_check_HDL(HDL_test_result, expected):
     from calculator import check_HDL
     answer = check_HDL(HDL_test_result)
-    assert answer = expected
+    assert answer == expected
 
